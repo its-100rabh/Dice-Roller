0a7a08aa64b8dbe5ff2821eb8920cc16d922cec2
its-100rabh <saurabhmahapatra03@gmail.com>
2022-09-20 11:55:12 +0530

    My first commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.*
+
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollbutton: Button = findViewById(R.id.roll_button)
+        rollbutton.setOnClickListener {
+            Toast.makeText(this ,"Button Clicked",Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+
+        diceImage = findViewById(R.id.dice_image_1)
+    }
+
+    private fun rollDice() {
+        //val resultText: TextView = findViewById(R.id.result_text)
+        //val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (Random().nextInt(6) + 1)
+        {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+        //val diceImage: ImageView = findViewById(R.id.dice_image_1) not memory efficient
+        diceImage.setImageResource(drawableResource)
+        //resultText.text= randomInt.toString()
+    }
+}
